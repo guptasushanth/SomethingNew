08a62b4d3138c81aaaea4adcb9160929a4ad4403
sushanth <sushanthgupta9563@gmail.com>
2024-11-20 17:36:41 +0530

    Updated the path for token

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/webhook", (req, res) => {
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token === VERIFY_TOKEN) {
+  if (mode && token === process.env.VERIFY_TOKEN) {
     console.log("Webhook verified!");
     res.status(200).send(challenge);
   } else {

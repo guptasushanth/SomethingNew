f18324eef5754590f602ce0d69faad31043ac129
sushanth <sushanthgupta9563@gmail.com>
2024-11-24 22:20:29 +0530

    updated redirect url

--- mystore.js
+++ mystore.js
@@ -2,7 +2,8 @@ const myStoreDetails = () => {
   const data = {
     CLIENT_ID: "mockClientID",
     CLIENT_SECRET: "mockClientSecret",
-    REDIRECT_URI: "http://localhost:5000/auth/callback",
+    REDIRECT_URI:
+      "http://ec2-65-2-120-70.ap-south-1.compute.amazonaws.com:5000/auth/callback",
   };
   return data;
 };
